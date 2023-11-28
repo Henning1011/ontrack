3c73cdd17b79daf335a9a0a573218056eee85997
Henning Stender <henning.stender@icloud.com>
2023-11-28 17:56:39 +0100

    Connection with rSpotify is established, testing undone

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def spotify
     spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
+    redirect_to root_path, notice: "redirected"
     # Now you can access user's private data, create playlists and much more
   end
 end
