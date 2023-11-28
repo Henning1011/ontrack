be3224afda3da139608a9c2a5d5a9a3909cfee9c
Henning Stender <henning.stender@icloud.com>
2023-11-28 15:23:22 +0100

    Added Associations and Validations

--- app/models/playlist_track.rb
+++ app/models/playlist_track.rb
@@ -1,4 +1,6 @@
 class PlaylistTrack < ApplicationRecord
   belongs_to :track
   belongs_to :playlist
+
+  validates_uniqueness_of :track_id, :scope => [:playlist_id]
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -1,2 +1,7 @@
 class Track < ApplicationRecord
+  has_many :playlist_tracks
+  has_many :playlists, through: :playlist_tracks
+
+  validates :spotify_id, presence: true, uniqueness: true
+  validates :name, presence: true
 end
