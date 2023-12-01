726c10c9c214b6002a09a306ec196dbbeef4c111
Samuel Metz <metz.samuel@web.de>
2023-12-01 13:07:39 +0100

    We are getting recommendations

--- app/javascript/controllers/sliders_controller.js
+++ app/javascript/controllers/sliders_controller.js
@@ -2,21 +2,53 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="sliders"
 export default class extends Controller {
-  static targets = ["acousticness", "danceability", "energy", "instrumentalness", "liveness", "loudness", "speechiness", "valence", "popularity"]
+  static targets = ["acousticness", "danceability", "energy", "instrumentalness", "liveness", "loudness", "speechiness", "valence", "popularity", "results"]
+  static values = { id: String }
 
   connect() {
     console.log("hello")
   }
 
-  getvalues() {
-    const acousticness = this.acousticnessTarget.value
-    console.log(this.danceabilityTarget.value)
-    console.log(this.energyTarget.value)
-    console.log(this.instrumentalnessTarget.value)
-    console.log(this.livenessTarget.value)
-    console.log(this.loudnessTarget.value)
-    console.log(this.speechinessTarget.value)
-    console.log(this.valenceTarget.value)
-    console.log(this.popularityTarget.value)
-  }
+  search(event) {
+    const spotifyId = this.idValue
+    const v_acousticness = this.acousticnessTarget.value
+    const v_danceability = this.danceabilityTarget.value
+    const v_energy = this.energyTarget.value
+    const v_instrumentalness = this.instrumentalnessTarget.value
+    const v_liveness = this.livenessTarget.value
+    const v_loudness = this.loudnessTarget.value
+    const v_speechiness = this.speechinessTarget.value
+    const v_valence = this.valenceTarget.value
+    const v_popularity = this.popularityTarget.value
+
+    const url = `https://api.spotify.com/v1/recommendations?limit=10&seed_tracks=${spotifyId}&target_acousticness=${v_acousticness}&target_danceability=${v_danceability}&target_energy=${v_energy}&target_instrumentalness=${v_instrumentalness}&target_liveness=${v_liveness}&target_loudness=${v_loudness}&target_popularity=${v_popularity}&target_speechiness=${v_speechiness}&target_valence=${v_valence}`
+    fetch(url, {
+      method: "GET",
+      headers: { "Authorization": "Bearer BQA-vtYxsQ9FYoocLD_xhQtf1F8ffU_75iXQKvWTO6M_1kO0pQ6LoVonMVUqtdMmbXVyML2UzGL5MtYmpP1rjpIXiV3AtXFX68R0r73czJROnMyH_ho" },
+    })
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data)
+      const iframeTemplate = `
+      <div class="col-sm-8 my-3">
+      <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/${data}?utm_source=generator" width="100%" height="250" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    </div>
+      `
+      
+      })
+    }
+
+  // private
+
+  // #getvalues() {
+  //   const v_acousticness = this.acousticnessTarget.value
+  //   const v_danceability = this.danceabilityTarget.value
+  //   const v_energy = this.energyTarget.value
+  //   const v_instrumentalness = this.instrumentalnessTarget.value
+  //   const v_liveness = this.livenessTarget.value
+  //   const v_loudness = this.loudnessTarget.value
+  //   const v_speechiness = this.speechinessTarget.value
+  //   const v_valence = this.valenceTarget.value
+  //   const v_popularity = this.popularityTarget.value
+  // }
 }
