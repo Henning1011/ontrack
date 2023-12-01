db323718ef0cfa03641c614816bd23a1746739db
Samuel Metz <metz.samuel@web.de>
2023-12-01 15:17:14 +0100

    We are getting search results

--- app/javascript/controllers/sliders_controller.js
+++ app/javascript/controllers/sliders_controller.js
@@ -6,7 +6,6 @@ export default class extends Controller {
   static values = { id: String }
 
   connect() {
-    console.log("hello")
   }
 
   search(event) {
@@ -24,31 +23,18 @@ export default class extends Controller {
     const url = `https://api.spotify.com/v1/recommendations?limit=10&seed_tracks=${spotifyId}&target_acousticness=${v_acousticness}&target_danceability=${v_danceability}&target_energy=${v_energy}&target_instrumentalness=${v_instrumentalness}&target_liveness=${v_liveness}&target_loudness=${v_loudness}&target_popularity=${v_popularity}&target_speechiness=${v_speechiness}&target_valence=${v_valence}`
     fetch(url, {
       method: "GET",
-      headers: { "Authorization": "Bearer BQA-vtYxsQ9FYoocLD_xhQtf1F8ffU_75iXQKvWTO6M_1kO0pQ6LoVonMVUqtdMmbXVyML2UzGL5MtYmpP1rjpIXiV3AtXFX68R0r73czJROnMyH_ho" },
+      headers: { "Authorization": "Bearer BQCQ0OUT9lFamrIBjJunWb4hVnltoEcR94sUxKXIYeEAHOr0wHAmTeX8SnHLpZKBpK669k7-v-m5eTpFjd-R36GFe0yVHJZrK4VM9ZBuXAUk6TJkVng" },
     })
     .then(response => response.json())
     .then((data) => {
-      console.log(data)
-      const iframeTemplate = `
-      <div class="col-sm-8 my-3">
-      <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/${data}?utm_source=generator" width="100%" height="250" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-    </div>
-      `
-      
-      })
-    }
-
-  // private
-
-  // #getvalues() {
-  //   const v_acousticness = this.acousticnessTarget.value
-  //   const v_danceability = this.danceabilityTarget.value
-  //   const v_energy = this.energyTarget.value
-  //   const v_instrumentalness = this.instrumentalnessTarget.value
-  //   const v_liveness = this.livenessTarget.value
-  //   const v_loudness = this.loudnessTarget.value
-  //   const v_speechiness = this.speechinessTarget.value
-  //   const v_valence = this.valenceTarget.value
-  //   const v_popularity = this.popularityTarget.value
-  // }
+      data.tracks.forEach((track) => {
+        const iframeTemplate = `
+        <div class="my-3">
+          <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/${track.id}?utm_source=generator" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        </div>
+        `
+        this.resultsTarget.insertAdjacentHTML("beforeend", iframeTemplate)
+      });
+    })
+  }
 }
